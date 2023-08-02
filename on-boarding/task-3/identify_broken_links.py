5b84657c0645e336e0b14c876da222dc804d1e28
Leul Wujira <meraf000@gmail.com>
2023-08-02 14:04:07 +0300

    feat: add broken link identifier

---
+++ on-boarding/task-3/identify_broken_links.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+
+def is_alive(url):
+    try:
+        return 100 <= requests.get(url).status_code <= 399
+    except:
+        return False
+
+
+def get_broken_urls(site):
+    response = requests.get(site)
+    parsed = BeautifulSoup(response.text, 'lxml')
+
+    a_tags = parsed.find_all('a', href=True)
+
+    links = set()
+
+    for a_tag in a_tags:
+        absolute_url = urljoin(site, a_tag['href'])
+
+        if not is_alive(absolute_url):
+            links.add(absolute_url)
+
+    return list(links)
